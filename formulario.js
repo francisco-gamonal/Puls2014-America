c26011fd240c5f490a767d8da2d8b4a294ec1c5b
Francisco Gamonal <hfgamonalb@gmail.com>
2014-02-04 13:36:36 -0500

    Commit 05 Clase Final

--- formulario.js
+++ formulario.js
@@ -1,34 +1,49 @@
-var $form = $('#formulario'),
-    $titulo = $('#titulo'),
-    $url = $('#url'),
-    $button = $('#mostrar-form'),
-    $list = $('#contenido'),
-    $post = $('.item').first();
+var $form  = $('#formulario'),
+	$titulo = $('#titulo'),
+	$url = $('#link'),
+	$primerPost = $('.item').first(),
+	$lista = $("#contenido");
 
 
-function mostrarFormulario(){
+if (localStorage.getItem('autosave'))
+{
+	$titulo.val(sessionStorage.getItem('titulo'));
+	$url.val(sessionStorage.getItem('url'));
+}
+
+
+var id = setInterval( function(){
+	sessionStorage.setItem('titulo', $titulo.val());
+	sessionStorage.setItem('url', $url.val());
+},1000);
+
+
+function mostrarOcultarFormulario(){
 	$form.slideToggle();
+	$lista.slideToggle();
 	return false;
 }
+
 
 function agregarPost(){
-	var url = $url.val(),
-	    titulo = $titulo.val(),
-	    $clone = $post.clone();
+	var titulo = $titulo.val(),
+		url = $url.val(),
+		clone = $primerPost.clone();
 
-	$clone.find('.titulo_item a')
+	clone.find('.titulo_item a') 
 		.text(titulo)
 		.attr('href', url);
+	
+	clone.hide();
 
-	$clone.hide(); //es como un display none
-
-	$list.prepend($clone); // agregar un clone antes del elemento
-
-	$clone.fadeIn();
-
+	$lista.prepend(clone);
+	mostrarOcultarFormulario();
+	$titulo.val("");
+	$url.val("");
+	clone.slideDown();
 	return false;
 }
 
-// Eventos
-$button.click(mostrarFormulario);
-$form.on('submit', agregarPost);
+
+$('#publicar_nav a').click( mostrarOcultarFormulario );
+$('#formulario').on('submit', agregarPost)
